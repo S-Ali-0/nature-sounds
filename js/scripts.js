51277dad09090d07811e9fb8f3d780f74679d082
S-Ali-0 <118998558+S-Ali-0@users.noreply.github.com>
2023-01-02 01:11:27 +0000

    new image and added animation transition to prevent screen flash on background change

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,9 @@ const forestbtn = document.getElementById('forest');
 
 oceanbtn.addEventListener('click', function onClick(event) {
   
-    document.body.style.background = "url('./images/1.jpg') no-repeat center center fixed";
+    document.body.style.background = "url('./images/6.jpg') no-repeat center center fixed";
     document.body.style.backgroundSize = 'cover';
+    document.body.style.transition = '0.01s';
 
 });
 
@@ -13,11 +14,13 @@ rainbtn.addEventListener('click', function onClick(event) {
   
     document.body.style.background = "url('./images/3.jpg') no-repeat center center fixed";
     document.body.style.backgroundSize = 'cover';
+    document.body.style.transition = '0.01s';
 });
 
 forestbtn.addEventListener('click', function onClick(event) {
   
     document.body.style.background = "url('./images/5.jpg') no-repeat center center fixed";
     document.body.style.backgroundSize = 'cover';
+    document.body.style.transition = '0.01s';
 
 });
