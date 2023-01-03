bd419068f334f82e442e93827b504f1c64e22707
S-Ali-0 <118998558+S-Ali-0@users.noreply.github.com>
2023-01-03 21:46:11 +0000

    javascript functions to toggle container visibility and icon classes

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ const seabtn = document.getElementById('sea');
 const rainbtn = document.getElementById('rain');
 const forestbtn = document.getElementById('forest');
 
+
+
 seabtn.addEventListener('click', function onClick(event) {
   
     document.body.style.background = "url('./images/3.jpg') no-repeat center center fixed";
@@ -24,3 +26,21 @@ forestbtn.addEventListener('click', function onClick(event) {
     document.body.style.transition = '0.01s';
 
 });
+
+function toggleDiv() {
+    let x = document.getElementById("nature");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function playBtn() {
+    const element = document.getElementById("toggle-icon");
+    if (element.className == "ri-play-fill") {
+      element.className = "ri-pause-circle-fill";
+    } else {
+      element.className = "ri-play-fill";
+    }
+  }
